26cef0bece1d67d272d868c01ae6eb6ea3d71bf1
juwenzhang <3137536916@qq.com>
2024-10-25 03:26:35 +0800

    feat: 请求数据和初步解析数据的实现

--- project/getDataFromWangYiNews.py
+++ project/getDataFromWangYiNews.py
@@ -1,3 +1,6 @@
+import sys
+from pkgutil import get_data
+
 import requests
 
 
@@ -21,27 +24,56 @@ class GetDataFromWangYi(object):
         print("本次爬取任务完毕...")
 
 
-    def get_data(self):
+    def remove_char(self, content, del_char, replace_char, *args, **kwargs) -> str:
+        """
+        用来实现对返回数据的字符串实现处理的函数
+        :param content:
+        :param del_char:
+        :param replace_char:
+        :param args:
+        :param kwargs:
+        :return: content.replace(del_char, replace_char)
+        """
+        return content.replace(del_char, replace_char)
+
+
+    def get_data(self, *args, **kwargs):
         """获取数据"""
-        pass
+        try:
+            response = requests.get(self.url, headers=self.headers).text
+            return response
+
+        except Exception as e:
+            print(f"error: {e}")
+            # 实现设置递归深度
+            sys.setrecursionlimit(20)
+            self.get_data()
 
 
-    def parse_data(self):
-        """解析数据"""
-        pass
+
+    def parse_data(self, data, *args, **kwargs):
+        """
+        解析数据: 将响应报文中的数据实现处理成 json 数据
+        :param data:
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        first_data = self.remove_char(data, 'data_callback(', "")
+        finally_data = self.remove_char(first_data, ')', "")
+        print(finally_data)
 
 
-    def save_data(self):
+    def save_data(self, *args, **kwargs):
         """保存数据"""
         pass
 
 
-    def run(self):
-        """运行程序"""
-        pass
+    def run(self, *args, **kwargs):
+        """启动程序"""
+        self.parse_data(self.get_data())
 
 
 if __name__ == "__main__":
     getDataFromWangYi = GetDataFromWangYi()
-
     getDataFromWangYi.run()
