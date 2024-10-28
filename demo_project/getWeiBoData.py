553d120080513270462b55f9f3845dd3fa6383a2
juwenzhang <3137536916@qq.com>
2024-10-29 04:07:54 +0800

    feat: 爬取微博评论数据的爬虫架初步搭建

---
+++ demo_project/getWeiBoData.py
@@ -0,0 +1,60 @@
+import requests
+
+
+class GetWeiBoComments(object):
+    def __init__(self, *args, **kwargs):
+        """
+        构造函数
+        :param args: 可选参数
+        :param kwargs: 可选参数
+        """
+        pass
+
+    def __del__(self, *args, **kwargs):
+        """
+        析构函数书写
+        :param args: 可选参数
+        :param kwargs: 可选承诺书
+        :return: None
+        """
+        print("此次爬取任务完毕")
+
+    def get_data(self, *args, **kwargs):
+        """
+        获取数据
+        :param args: 可选参数
+        :param kwargs: 可选参数
+        :return: None
+        """
+        pass
+
+    def parse_data(self, *args, **kwargs):
+        """
+        解析数据
+        :param args: 可选参数
+        :param kwargs: 可选参数
+        :return: None
+        """
+        pass
+
+    def save_data(self, *args, **kwargs):
+        """
+        保存数据
+        :param args: 可选参数
+        :param kwargs: 可选参数
+        :return: None
+        """
+        pass
+
+    def run(self, *args, **kwargs):
+        """
+        运行函数
+        :param args: 可选参数
+        :param kwargs: 可选参数
+        :return: None
+        """
+        pass
+
+if __name__ == "__main__":
+    getWeiBoComments = GetWeiBoComments()
+    getWeiBoComments.run()
