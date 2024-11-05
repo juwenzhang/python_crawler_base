c7f2be3740ecd285a61537c72ccf28f038f792b2
juwenzhang <3137536916@qq.com>
2024-11-05 17:05:16 +0800

    feat: selenium二次提交

--- demo_project/get_JD_selenium_data.py
+++ demo_project/get_JD_selenium_data.py
@@ -24,7 +24,11 @@ class GetJDSelenium(object):
         """
         print("自动化测试任务完毕...")
 
-    def get_jd(self):
+    def selenium_search(self):
+        """
+        用来实现搜索东西的自动化测试函数
+        :return:
+        """
         self.browser.get(self.url)
         # 直接先实现满屏操作
         self.browser.maximize_window()
@@ -36,14 +40,37 @@ class GetJDSelenium(object):
         text_input.send_keys(self.content)
         text_input.send_keys(Keys.ENTER)
 
-    def get_data(self):
+    def selenium_login(self):
+        """
+        用来解决登录问题的函数
+        :return:
+        """
         # 等待页面的加载完毕
-        pass
+        self.browser.execute_script('window.scrollTo(0, document.body.scrollHeight)')  # 到底部
+        try:
+            login_btn = self.browser.find_element(By.XPATH, '//*[@id="J_main"]/div[4]/div/div[2]')
+            login_btn.click()
+            # =================================================
+            time.sleep(10)
+            use_weixin_login = self.browser.find_element(By.CSS_SELECTOR, '#kbCoagent > ul > li:nth-child(3) > a > span')
+            use_weixin_login.click()
+            # =================================================
+            time.sleep(10)
+            weixin_btn = self.browser.find_element(By.XPATH, '//*[@id="tpl_for_page"]/span[1]/div[1]/div[5]/div/button')
+            weixin_btn.click()
+        except Exception as e:
+            print(f"error: {e}")
 
 
     def run(self):
-        self.get_jd()
-        time.sleep(20)
+        """
+        运行函数
+        :return:
+        """
+        self.selenium_search()
+        time.sleep(10)
+        self.selenium_login()
+        time.sleep(10)
         self.browser.close()
 
 if __name__ == "__main__":
